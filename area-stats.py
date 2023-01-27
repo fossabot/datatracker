41ef7115b8d971802ecda961ab74b65523119355
Lars Eggert <lars@eggert.org>
2023-01-27 16:11:42 +0200

    Track ADs

--- area-stats.py
+++ area-stats.py
@@ -29,19 +29,29 @@ def fmt_acronym(group):
     return group.acronym if group.state.slug == "active" else f"({group.acronym})"
 
 
-def mk_row(name, docs, pages, parent):
+def mk_row(name, docs, pages, parent, area=None):
     return pd.DataFrame.from_records(
-        [(name, docs, pages, parent)],
-        columns=["name", "docs", "pages", "parent"],
+        [(name, docs, pages, parent, area if area else parent)],
+        columns=["name", "docs", "pages", "parent", "area"],
     )
 
 
-def mk_row_group(group, group_docs):
+def mk_row_group(group, group_docs, ad=None, parent=None):
     return mk_row(
         fmt_acronym(group),
         group_docs.count() or 0,
         group_docs.aggregate(Sum("pages"))["pages__sum"] or 0,
+        fmt_acronym(group.parent) if not ad else ad,
         fmt_acronym(group.parent),
+    )
+
+
+def mk_row_ad(ad_name, ad_docs, area):
+    return mk_row(
+        ad_name,
+        ad_docs.count() or 0,
+        ad_docs.aggregate(Sum("pages"))["pages__sum"] or 0,
+        fmt_acronym(area),
     )
 
 
@@ -58,6 +68,16 @@ docs = (
 
 
 df = pd.DataFrame()
+ads = {}
+
+
+def add_ad(ad_name, area):
+    global df, ads
+    ads[ad_name] = area
+    ad_docs = area_docs.filter(group__role__name="ad", group__role__person=ad)
+    df = pd.concat([df, mk_row_ad(ad_name, ad_docs, a)], ignore_index=True)
+
+
 for a in Group.objects.filter(type="area"):
     area_docs = docs.filter(group__parent=a).exclude(group__acronym="none")
     if not area_docs:
@@ -71,7 +91,18 @@ for a in Group.objects.filter(type="area"):
         if not wg_docs:
             continue
 
-        df = pd.concat([df, mk_row_group(wg, wg_docs)], ignore_index=True)
+        ad = wg.ad_role().person
+        ad_name = ad.first_name()
+        if ad_name not in ads:
+            add_ad(ad_name, a.acronym)
+        elif ads[ad_name] != a.acronym:
+            ad_name = f"{ad_name} "  # FIXME: breaks if an AD is in three areas; use ({a.acronym})
+            if ad_name not in ads:
+                add_ad(ad_name, a.acronym)
+
+        df = pd.concat(
+            [df, mk_row_group(wg, wg_docs, ad_name, a.acronym)], ignore_index=True
+        )
 
         for doc in wg_docs:
             name = (
@@ -79,7 +110,6 @@ for a in Group.objects.filter(type="area"):
                 if doc.rfc_number()
                 else doc.name  # .replace("draft-", "")
             )
-            # print(wg.parent.acronym, wg.acronym, name, doc.get_state_slug())
             df = pd.concat(
                 [df, mk_row(name, 1, doc.pages, fmt_acronym(wg))], ignore_index=True
             )
@@ -116,7 +146,7 @@ def sunburst(types):
 
 
 def sankey(types, changes):
-    areas = df[df["parent"] == "iesg"]
+    areas = df[df["area"] == "iesg"]
 
     key = {y: x for x, y in enumerate(areas["name"])}
     new_key = {y: x + len(key) for x, y in enumerate(areas["name"])}
@@ -138,7 +168,7 @@ def sankey(types, changes):
             row = df[df["name"] == group]
             if row.empty:
                 continue
-            old_area = row["parent"].iloc[0]
+            old_area = row["area"].iloc[0]
             value = row[label].iloc[0]
             sources.append(key[old_area])
             if new_area not in new_key:
